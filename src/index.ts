ec69e073d5f42e0f83432fc98293bb8dda78e63b
georgelucasgp <djgeorgelucas@hotmail.com>
2022-07-29 18:56:52 -0300

    add express

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-const a:string = 'Typescript'
+import express from 'express'
 
-console.log(a);
+const app = express()
+
+app.get('', (req,res) => {
+  res.send('Hello Word!')
+})
+const port = 3333
+
+app.listen(port,() => {
+  console.log(`Server is runner on port ${port}`)
+})
